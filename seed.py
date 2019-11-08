e1d5393ae49dbf94ccaeb954a681a54540c5a4e2
MWQ <michelle.w.qu@gmail.com>
2019-11-08 07:52:09 +0000

    Creates tables and populates media types

---
+++ seed.py
@@ -0,0 +1,24 @@
+from model import (User, Media, Page, ReactVar, MediaType, WhichTag, 
+                   Tag, Like, Following)
+from model import connect_to_db, db
+from server import app
+from datetime import datetime
+
+def set_acceptable_media_types():
+    acceptable_list = [MediaType(media_ext='gif'),
+                       MediaType(media_ext='jpg'),
+                       MediaType(media_ext='jpeg'),
+                       MediaType(media_ext='png'),
+                       MediaType(media_ext='obj'),
+                       MediaType(media_ext='gltf')]
+    db.session.add_all(acceptable_list)
+    db.session.commit()
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Run functions
+    set_acceptable_media_types()
